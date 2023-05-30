56790bc469f3410dd11998837de50e42efdb4b31
Mohd-PH <9362157+Mohd-PH@users.noreply.github.com>
2023-05-30 16:17:42 +0300

    added telegraf to create telegram bot

--- index.js
+++ index.js
@@ -1,34 +1,65 @@
 const { User, Region } = require("./models/index.js");
+require("./env.js");
+const { Telegraf } = require("telegraf");
+const { message } = require("telegraf/filters");
 
 const main = async () => {
-  const randomNumber = Math.floor(Math.random() * 1231231);
-  const user1 = await User.query().insertGraph({
-    telegram_user_id: randomNumber,
-    username: "user 1",
-    name: "user1",
-    chat_id: randomNumber,
-  });
-  console.log(`Created: ${JSON.stringify(user1)}`);
+  const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-  const region1 = await Region.query().insertGraph({
-    name: `Central region ${randomNumber}`,
-  });
-  console.log(`Created: ${JSON.stringify(region1)}`);
-
-  const location1 = await region1.$relatedQuery("locations").insert({
-    name: `Riyadh ${randomNumber}`,
-    region: region1,
+  bot.command("quit", async (ctx) => {
+    console.log("quitted");
+    await ctx.leaveChat();
   });
 
-  const opportunity1 = await location1.$relatedQuery("opportunities").insert({
-    source: "MOH",
-    source_id: "2334",
-    title: "aaaaa",
+  bot.start(async (ctx) => {
+    await ctx.reply("حياك الله " + ctx.message.from.first_name + "!");
+    ctx.message.from.first_name;
+    let user = await User.query().findOne({
+      telegram_user_id: ctx.message.from.id,
+      chat_id: ctx.message.chat.id,
+    });
+    console.log(user);
+    if (!user) {
+      user = await User.query().insertGraph({
+        name: ctx.message.from.first_name,
+        username: ctx.message.from.username,
+        telegram_user_id: ctx.message.from.id,
+        chat_id: ctx.message.chat.id,
+      });
+      console.log(`Created user: ${JSON.stringify(user)}`);
+    } else {
+      console.log(`User already created: ${JSON.stringify(user)}`);
+    }
   });
 
-  // await user1.$relatedQuery("locations").relate(location1);
-  await opportunity1.$relatedQuery("users").relate(user1);
-  console.log(`Created: ${JSON.stringify(location1)}`);
+  bot.action("edit-locations", async (ctx) => {
+    await ctx.reply("editing locations reply");
+    await ctx.answerCbQuery("editing locations cb query");
+  });
+
+  bot.command("settings", async (ctx) => {
+    ctx.reply("اعداداتك", {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "المدن",
+              callback_data: "edit-locations",
+            },
+            {
+              text: "المناطق",
+              callback_data: "edit-regions",
+            },
+          ],
+        ],
+      },
+    });
+  });
+
+  bot.launch();
+  process.once("SIGINT", () => bot.stop("SIGINT"));
+  process.once("SIGTERM", () => bot.stop("SIGTERM"));
+
   return;
 };
 
