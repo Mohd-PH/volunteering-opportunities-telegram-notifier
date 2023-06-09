1330963b30745ae89492333a056bb524a134b24c
Mohd-PH <9362157+Mohd-PH@users.noreply.github.com>
2023-06-09 10:48:17 +0300

    Add host and port to production config

--- knexfile.js
+++ knexfile.js
@@ -44,6 +44,8 @@ module.exports = {
     client: process.env.DATABASE_CLIENT,
     connection: {
       filename: "./dev.sqlite3",
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
       database: process.env.DATABASE_NAME,
       user: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
